cb7460e46286956c564a911aa5459e75c40973ef
FaabScheeren <f.scheeren@outlook.com>
2021-04-12 15:41:50 +0200

    Iniitial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+// import HomeIcon from "@bit/frankwatching.fw_7_test.icons.home-icon";
+import Button from "@bit/frankwatching.fw_7_test.molecules.button";
+import Checkbox from "@bit/frankwatching.fw_7_test.molecules.checkbox";
+import Inputfield from "@bit/frankwatching.fw_7_test.molecules.inputfield";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          backgroundColor: "#019000",
+          height: "8px",
+          width: "100%",
+          position: "fixed",
+          top: 0,
+        }}
+      ></div>
+      <div className="container">
+        <div>
+          <h1>Dit zijn je opties</h1>
+          <p>Vink hier en daar iets leuks aan en vul je gegevens in.</p>
+          <form>
+            <span className="checkbox_group_label" style={{ textAlign: "left" }}>Maak een selectie</span>
+            <Checkbox
+              name="yes"
+              for="yes_option"
+              labelText="Wow ja deze wil ik!"
+            />
+            <Checkbox name="no" for="no_option" labelText="Deze optie stinkt" />
+
+            <Inputfield
+              placeholder="Vul hier iets leuks in"
+              label="E-mailadres"
+            />
+            <Button
+              classes="btn-primary fixed_width"
+              iconRight="arrow-right"
+              text="Kies deze button"
+            />
+            <Button
+              classes="btn-secundary fixed_width"
+              iconLeft="home"
+              text="Of kies deze button"
+            />
+            <Button
+              classes="btn-tertiary fixed_width"
+              text="Ik weet het niet"
+            />
+            <Button
+              classes="btn-textLink fixed_width"
+              text="Ik heb geen idee wat ik hier doe"
+            />
+            <Button classes="btn-primary" text="Mand" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+
+module.exports = {
+  devtool: "source-map",
+
+  // entry: {
+  //   "js/index": path.resolve(__dirname, "src/index.js"),
+  //   "js/index.min": path.resolve(__dirname, "src/index.js"),
+  // },
+
+  // output: {
+  //   filename: "[name]-[hash:6].js",
+  //   path: path.resolve(__dirname, "build"),
+  // },
+
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              // presets: ['es2015'],
+              plugins: [require("babel-plugin-transform-async-to-promises")],
+            },
+          },
+        ],
+        // loader : 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader", // compiles Sass to CSS
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        loader: "url-loader",
+      },
+    ],
+  },
+};
